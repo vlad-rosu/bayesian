edc4c36135efc883f8aeabad4eba47c842b8af57
mariah <mariahachie@gmail.com>
2017-11-20 20:08:05 -0500

    Scrolling off by 1px

--- js/creative.js
+++ js/creative.js
@@ -8,7 +8,6 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 101)
         }, 1000, "easeInOutExpo");
         return false;
       }

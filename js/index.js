70c6f2c28b0ec192f4aa6831530cefd54d61af18
mariah <mariahachie@gmail.com>
2017-11-25 22:11:48 -0500

    clean up code, fix days, and default values

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
+var countDownDate = new Date("Jan 20, 2013 15:37:25").getTime();
 
 // Create Countdown
 var Countdown = {
@@ -10,18 +10,6 @@ var Countdown = {
 
   // Initialize the countdown  
   init: function() {
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var initDays = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var initHours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var initMinutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var initSeconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
     // DOM
 	this.$ = {
         days   : this.$el.find('.bloc-time.days .figure'),
@@ -32,10 +20,10 @@ var Countdown = {
 
     // Init countdown values
     this.values = {
-        days  : initDays,
-	    hours  : initHours,
-        minutes: initMinutes,
-        seconds: initSeconds,
+        days  : 0,
+	    hours  : 0,
+        minutes: 0,
+        seconds: 0
     };
 
     // Animate countdown to the end 
@@ -66,19 +54,15 @@ var Countdown = {
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-        // // Output the result in an element with id="demo"
-        // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-        // + minutes + "m " + seconds + "s ";
 
         // If the count down is over, write some text 
         if (distance < 0) {
             clearInterval(that.countdown_interval);
-            document.getElementById("demo").innerHTML = "EXPIRED";
+            // document.getElementById("demo").innerHTML = "EXPIRED";
         } else {
         // Update DOM values
         // Days
-        // that.checkHour(days, $day_1, $day_2);
+        that.checkHour(days, $day_1, $day_2);
 
         // Hours
         that.checkHour(hours, $hour_1, $hour_2);
